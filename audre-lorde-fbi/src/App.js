2fa6b80ff154623068e85cad41dc3599e97950c2
lmalexander <elizabeth.alexander@northwestern.edu>
2020-11-14 12:56:42 -0600

    created and imported components and pages into app.js, added react-router-dom and created routes in app.js

--- audre-lorde-fbi/src/App.js
+++ audre-lorde-fbi/src/App.js
@@ -1,11 +1,52 @@
+// -------------------- react
+import React, {Component} from 'react';
+// -------------------- react bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+// -------------------- react router
+import {
+  BrowserRouter as Router,
+  // Switch,
+  Route,
+  // Link
+} from "react-router-dom";
+// ------------------- pages
+import Home from "./pages/Home";
+import Background from "./pages/Background";
+import Files from "./pages/Files";
+import FOIA from "./pages/FOIA";
+import Citations from "./pages/Citations";
+import Contact from "./pages/Contact";
 
-function App() {
-  return (
-    <div className="container">
-          <h2>NEW YORK 100-122142 AUDRE LORDE FBI FILES</h2>
-        </div>
+// ------------------- components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+class App extends Component {
+
+  render() {
+    return (
+    <Router> 
+      <div>
+        <Container>
+        <h2>NEW YORK 100-122142 AUDRE LORDE FBI FILES</h2>
+        <Header />
+        
+        <Route path="/" exact component={Home} />
+        <Route path="/background" exact component={Background} />
+        <Route path="/files" exact component={Files} />
+        <Route path="/foia-request" exact component={FOIA} />
+        <Route path="/citations" exact component={Citations} />
+        <Route path="/contact-lma" exact component={Contact} />
+
+        <Footer />
+        </Container>
+      </div> 
+    </Router>
   );
+  };
 }
 
 export default App;
