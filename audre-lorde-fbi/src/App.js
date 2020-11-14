3d73f48d425bcc0a3f629e4001e3529f6b017864
lmalexander <elizabeth.alexander@northwestern.edu>
2020-11-14 11:57:07 -0600

    basic reactapp structure with header

---
+++ audre-lorde-fbi/src/App.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="container">
+          <h2>NEW YORK 100-122142 AUDRE LORDE FBI FILES</h2>
+        </div>
+  );
+}
+
+export default App;
